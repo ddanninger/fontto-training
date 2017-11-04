af403a3fd2679a0c749464b10437f0d5f40f9fde
장상현 <jexjang92@gmail.com>
2017-11-04 21:23:39 +0900

    스크립트 변경
    ---
    Redmine Issue Progress / ID / Time :  100% #97 @1h
    ---
    What :
    - test : 상대경로로 변경
    - train : 상대경로로 변경
    - train 배치 180, 에폭1000으로 변경
    Why :
    -
    ---
    *Signed-off-by: sjang42 <jexjang92@gmail.com>*

--- test.sh
+++ test.sh
@@ -4,4 +4,4 @@ if [ $# -lt 1 ]; then
 fi
 
 data_root=./testsets/`echo $1 | cut -f 1 -d'_'`
-python test.py --dataroot $data_root --name B9DD_B9D0_output_written_512_trimed_256_rot_pix2pix --model test --which_model_netG unet_256 --which_direction AtoB --dataset_mode single --norm batch --how_many 100 --resize_or_crop=''
+python test.py --dataroot $data_root --name $1_pix2pix --model test --which_model_netG unet_256 --which_direction AtoB --dataset_mode single --norm batch --how_many 100 --resize_or_crop=''
--- train.sh
+++ train.sh
@@ -6,7 +6,7 @@ fi
 args=("$@")
 
 for ((i=0;i<$#;i++)); do
-        python datasets/combine_A_and_B.py --fold_A /home/sjang/work/fontto/pytorch-CycleGAN-and-pix2pix/datasets/${args[$i]}/A --fold_B /home/sjang/work/fontto/pytorch-CycleGAN-and-pix2pix/datasets/${args[$i]}/B --fold_AB /home/sjang/work/fontto/pytorch-CycleGAN-and-pix2pix/datasets/${args[$i]}/AB
-        python train.py --dataroot ./datasets/${args[$i]}/AB --name ${args[$i]}_pix2pix --model pix2pix --which_model_netG unet_256 --which_direction AtoB --lambda_A 100 --dataset_mode aligned --no_lsgan --no_flip --norm batch --pool_size 0 --display_id=0 --gpu_ids=0,1,2,3 --batchSize=91 --niter=2000 --niter_decay=0 --save_epoch_freq=500 --resize_or_crop=''
-        bash test.sh ${args[$i]}_pix2pix
+        python datasets/combine_A_and_B.py --fold_A datasets/${args[$i]}/A --fold_B datasets/${args[$i]}/B --fold_AB datasets/${args[$i]}/AB
+        python train.py --dataroot ./datasets/${args[$i]}/AB --name ${args[$i]}_pix2pix --model pix2pix --which_model_netG unet_256 --which_direction AtoB --lambda_A 100 --dataset_mode aligned --no_lsgan --no_flip --norm batch --pool_size 0 --display_id=0 --gpu_ids=0,1,2,3 --batchSize=180 --niter=1000 --niter_decay=0 --save_epoch_freq=300
+        bash test.sh ${args[$i]}
 done
