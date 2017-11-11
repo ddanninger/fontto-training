2b22337f679160997329fdc375541c0b1b448d3c
sjang42 <jexful92@google.com>
2017-11-11 15:52:15 +0900

    testsets 이름 변경 / 옵션 변경
    ---
    Redmine Issue Progress / ID / Time : 90 % #101 @10m
    ---
    What :
    - testsets -> testsets_256
    - loadSize 옵션 추가
    Why :
    트레인과 맞춰주기 위해
    ---
    *Signed-off-by: sjang42 <jexjang92@gmail.com>*

--- test.sh
+++ test.sh
@@ -3,5 +3,5 @@ if [ $# -lt 1 ]; then
   exit
 fi
 
-data_root=./testsets/`echo $1 | cut -f 1 -d'_'`
-python test.py --dataroot $data_root --name $1_pix2pix --model test --which_model_netG unet_256 --which_direction AtoB --dataset_mode single --norm batch --how_many 100 --resize_or_crop=''
+data_root=./testsets_256/`echo $1 | cut -f 1 -d'_'`
+python test.py --dataroot $data_root --name $1_pix2pix --model test --which_model_netG unet_256 --which_direction AtoB --dataset_mode single --norm batch --how_many 100 --resize_or_crop='' --input_nc=1 --output_nc=1 --loadSize=256 --fineSize=256
