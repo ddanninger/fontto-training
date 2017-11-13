1b5783516211e9696aba528599f2209c10afa84b
sjang42 <jexful92@google.com>
2017-11-13 14:43:38 +0900

    train.sh 기능 추가 / empry_trash.sh 추가
    ---
    Redmine Issue Progress / ID / Time :  100% #101 @2h
    ---
    What :
    - 폰트 이미지로부터 pix2pix데이터셋 만들기 기능 추가
    - 트레이닝 옵션 변경(채널1로 변경, 로드사이즈 256고정)
    - test 각주처리
    - pth -> S3 기능 추가
    - 학습 후 데이터셋과 pth삭제 및 trash로 이동 기능 추가
    - crontab을 위한 trash 비워주는 스크립트 구현
    
    Why :
    - 학습 자동화를 위해
    ---
    *Signed-off-by: sjang42 <jexjang92@gmail.com>*

---
+++ empty_trash.sh
@@ -0,0 +1,7 @@
+# ./trash의 모든 데이터를 지웁니다.
+
+
+# trash 디렉토리의 위치를 지정하고 아래 스크립트의 각주를 제거한 후 사용합니다.
+WHERE_TRASH=
+
+# rm -rf ${WHERE_TRASH}/trash/*
--- train.sh
+++ train.sh
@@ -1,12 +1,66 @@
-if [ $# -lt 1 ]; then
-  echo '$1 <Directory for training datasets>'
+if [ $# -lt 2 ]; then
+  echo '$1 <hangle A>, $2 <hangle B>, $3 <epoch>'
   exit
 fi
 
-args=("$@")
 
-for ((i=0;i<$#;i++)); do
-        python datasets/combine_A_and_B.py --fold_A datasets/${args[$i]}/A --fold_B datasets/${args[$i]}/B --fold_AB datasets/${args[$i]}/AB
-        python train.py --dataroot ./datasets/${args[$i]}/AB --name ${args[$i]}_pix2pix --model pix2pix --which_model_netG unet_256 --which_direction AtoB --lambda_A 100 --dataset_mode aligned --no_lsgan --no_flip --norm batch --pool_size 0 --display_id=0 --gpu_ids=0,1,2,3 --batchSize=180 --niter=1000 --niter_decay=0 --save_epoch_freq=300
-        bash test.sh ${args[$i]}
-done
+echo `date`
+printf "_____________start training [%s]->[%s], epoch [%s]_____________\n" $1 $2 $3
+# make dataset
+JPG_DIR=fonts_256
+DATASET_DIR=datasets
+CHECK_DIR=checkpoints
+
+printf "start copying [%s] to A\n" $1
+cp -r ${JPG_DIR}/$1 A
+mkdir A/train
+mv A/*.jpg A/train
+printf "done! copying [%s] to A\n" $1
+
+cp -r ${JPG_DIR}/$2 B
+printf "start copying [%s] to B\n" $2
+mkdir B/train
+mv B/*.jpg B/train
+printf "done! copying [%s] to B\n" $2
+
+UNICODE_A=`printf "%X" "'$1'"`
+UNICODE_B=`printf "%X" "'$2'"`
+OUTPUT_DIR=${UNICODE_A}_${UNICODE_B}_${JPG_DIR}
+
+printf "start making data [%s]\n" ${DATASET_DIR}/${OUTPUT_DIR}
+mkdir $OUTPUT_DIR
+mv A B $OUTPUT_DIR
+mv $OUTPUT_DIR $DATASET_DIR
+printf "done! making data [%s]\n" ${DATASET_DIR}/${OUTPUT_DIR}
+
+
+# convert dataset into aligned images
+printf "start converting data into algned images [%s]\n" ${DATASET_DIR}/${OUTPUT_DIR}/AB
+python ${DATASET_DIR}/combine_A_and_B.py --fold_A=${DATASET_DIR}/${OUTPUT_DIR}/A --fold_B=${DATASET_DIR}/${OUTPUT_DIR}/B --fold_AB=${DATASET_DIR}/${OUTPUT_DIR}/AB
+printf "done! converting data into algned images [%s]\n" ${DATASET_DIR}/${OUTPUT_DIR}/AB
+
+# training
+printf "start training"
+echo ${OUTPUT_DIR}_pix2pix
+python train.py --dataroot ${DATASET_DIR}/${OUTPUT_DIR}/AB --name ${OUTPUT_DIR}_pix2pix --model pix2pix --which_model_netG unet_256 --which_direction AtoB --lambda_A 100 --dataset_mode aligned --no_lsgan --no_flip --norm batch --pool_size 0 --display_id=0 --gpu_ids=0,1,2,3 --batchSize=60 --niter=$3 --niter_decay=0 --save_epoch_freq=$3 --loadSize=256 --fineSize=256 --input_nc=1 --output_nc=1 --slack_freq=200
+printf "done! training"
+python send_to_slack.py --msg="학습이 종료되었습니다." --channel=training
+
+
+# test
+# bash test.sh ${OUTPUT_DIR}
+
+
+# upload pth to S3
+aws s3 cp ${CHECK_DIR}/${OUTPUT_DIR}_pix2pix/$3_net_G.pth s3://fontto/data/pths/${UNICODE_A}/${UNICODE_A}_${UNICODE_B}/
+python send_to_slack.py --msg="`printf "[%s_%s/%s_net_G.pth]가 s3에 업로드 되었습니다." $UNICODE_A $UNICODE_B $3`" --channel=training
+
+
+# delete used data for space's sake
+rm -rf ${DATASET_DIR}/${OUTPUT_DIR}
+printf "removed [%s]\n" ${DATASET_DIR}/${OUTPUT_DIR}
+
+rsync -av --remove-source-files ${CHECK_DIR}/${OUTPUT_DIR}_pix2pix ./trash
+rm -rf ${CHECK_DIR}/${OUTPUT_DIR}_pix2pix
+printf "moved [%s] to trash\n" ${CHECK_DIR}/${OUTPUT_DIR}_pix2pix
+printf "_____________done! training [%s]->[%s], epoch [%s]_____________\n" $1 $2 $3
