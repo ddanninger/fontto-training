ca98235b2b906b07d26d190954caff7ca71c172f
sjang42 <jexful92@google.com>
2017-11-16 17:06:58 +0900

    visualizer 삭제
    ---
    Redmine Issue Progress / ID / Time :  100% #100 @1h
    ---
    What :
    - visualizer 삭제
    
    Why :
    - 속도 향상
    ---
    *Signed-off-by: sjang42 <jexjang92@gmail.com>*

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ import time
 from options.train_options import TrainOptions
 from data.data_loader import CreateDataLoader
 from models.models import create_model
-from util.visualizer import Visualizer
+# from util.visualizer import Visualizer
 from slackbot import slackbot
 
 opt = TrainOptions().parse()
@@ -12,7 +12,7 @@ dataset_size = len(data_loader)
 print('#training images = %d' % dataset_size)
 
 model = create_model(opt)
-visualizer = Visualizer(opt)
+# visualizer = Visualizer(opt)
 total_steps = 0
 time_start_training = time.time()
 total_epoch = opt.niter + opt.niter_decay
@@ -30,18 +30,18 @@ for epoch in range(opt.epoch_count, opt.niter + opt.niter_decay + 1):
         model.set_input(data)
         model.optimize_parameters()
 
-        if total_steps % opt.display_freq == 0:
-            visualizer.display_current_results(model.get_current_visuals(),
-                                               epoch)
+        # if total_steps % opt.display_freq == 0:
+        #     visualizer.display_current_results(model.get_current_visuals(),
+        #                                        epoch)
 
         if total_steps % opt.print_freq == 0:
             errors = model.get_current_errors()
             t = (time.time() - iter_start_time) / opt.batchSize
-            visualizer.print_current_errors(epoch, epoch_iter, errors, t)
-            if opt.display_id > 0:
-                visualizer.plot_current_errors(epoch,
-                                               float(epoch_iter) / dataset_size,
-                                               opt, errors)
+            # visualizer.print_current_errors(epoch, epoch_iter, errors, t)
+            # if opt.display_id > 0:
+            #     visualizer.plot_current_errors(epoch,
+            #                                    float(epoch_iter) / dataset_size,
+            #                                    opt, errors)
 
         if total_steps % opt.save_latest_freq == 0:
             print('saving the latest model (epoch %d, total_steps %d)' %
