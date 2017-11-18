eb98996ef111d5e7c535d541df5f2749503a268e
sjang42 <jexful92@google.com>
2017-11-18 15:45:19 +0900

    bash 지정 / 데이터셋 만드는 방법 변경 / train 옵션 인자로 변경 / pth s3전송 전 존재 확인
    ---
    Redmine Issue Progress / ID / Time :  100% #101 @1h
    ---
    What :
    - #!/usr/bin/env bash 추가
    - 이미 존재하는 데이터셋은 그대로 사용하도록 변경
    - num_treads, gpu_ids, slack_freq, batch_size 쉽게 변경할 수 있도록 변수로 등록
    - pth s3에 등록 전 파일 존재 유무 확인 후 없으면 올리지 않고 슬랙으로 실패 메세지 전송
    
    Why :
    - bash 사용을 명시
    - 데이터 만드는 시간을 효율적으로 사용
    - 각자의 환경에 맞게 쉽게 변경
    - pth 전송 실패를 알 수 있도록
    
    ---
    *Signed-off-by: sjang42 <jexjang92@gmail.com>*

--- train.sh
+++ train.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 if [ $# -lt 3 ]; then
   echo '$1 <hangle A>, $2 <hangle B>, $3 <epoch>'
   exit
@@ -6,44 +7,40 @@ fi
 
 echo `date`
 printf "_____________start training [%s]->[%s], epoch [%s]_____________\n" $1 $2 $3
-# make dataset
+# dataset argument
 JPG_DIR=fonts_256
 DATASET_DIR=datasets
 CHECK_DIR=checkpoints
 
-printf "start copying [%s] to A\n" $1
-cp -r ${JPG_DIR}/$1 A
-mkdir A/train
-mv A/*.jpg A/train
-printf "done! copying [%s] to A\n" $1
+# these values should be changed accordingly
+NUM_THREADS=6
+GPU_IDS=0,1,2,3
+SLACK_FREQ=200
+BATCH_SIZE=60
 
-cp -r ${JPG_DIR}/$2 B
-printf "start copying [%s] to B\n" $2
-mkdir B/train
-mv B/*.jpg B/train
-printf "done! copying [%s] to B\n" $2
 
 UNICODE_A=`printf "%X" "'$1'"`
 UNICODE_B=`printf "%X" "'$2'"`
 OUTPUT_DIR=${UNICODE_A}_${UNICODE_B}_${JPG_DIR}
 
-printf "start making data [%s]\n" ${DATASET_DIR}/${OUTPUT_DIR}
-mkdir $OUTPUT_DIR
-mv A B $OUTPUT_DIR
-mv $OUTPUT_DIR $DATASET_DIR
-printf "done! making data [%s]\n" ${DATASET_DIR}/${OUTPUT_DIR}
+# check if data exist
+# if not exist, make data in dataset
+if [ ! -d ${DATASET_DIR}/${OUTPUT_DIR} ]
+then
+  # convert dataset into aligned images
+  printf "start converting data into algned images [%s]\n" ${DATASET_DIR}/${OUTPUT_DIR}/AB
+  python ${DATASET_DIR}/combine_A_and_B.py --fold_A=${JPG_DIR}/$1 --fold_B=${JPG_DIR}/$2 --fold_AB=${DATASET_DIR}/${OUTPUT_DIR}/AB
+  printf "done! converting data into algned images [%s]\n" ${DATASET_DIR}/${OUTPUT_DIR}/AB
+else
+  printf "dataset [%s] already exist\n" ${DATASET_DIR}/${OUTPUT_DIR}/
+fi
 
-
-# convert dataset into aligned images
-printf "start converting data into algned images [%s]\n" ${DATASET_DIR}/${OUTPUT_DIR}/AB
-python ${DATASET_DIR}/combine_A_and_B.py --fold_A=${DATASET_DIR}/${OUTPUT_DIR}/A --fold_B=${DATASET_DIR}/${OUTPUT_DIR}/B --fold_AB=${DATASET_DIR}/${OUTPUT_DIR}/AB
-printf "done! converting data into algned images [%s]\n" ${DATASET_DIR}/${OUTPUT_DIR}/AB
 
 # training
-printf "start training"
+printf "start training\n"
 echo ${OUTPUT_DIR}_pix2pix
-python train.py --dataroot ${DATASET_DIR}/${OUTPUT_DIR}/AB --name ${OUTPUT_DIR}_pix2pix --model pix2pix --which_model_netG unet_256 --which_direction AtoB --lambda_A 100 --dataset_mode aligned --no_lsgan --no_flip --norm batch --pool_size 0 --display_id=0 --gpu_ids=0,1,2,3 --batchSize=60 --niter=$3 --niter_decay=0 --save_epoch_freq=$3 --loadSize=256 --fineSize=256 --input_nc=1 --output_nc=1 --slack_freq=200 --no_html --nThreads=6
-printf "done! training"
+python train.py --dataroot ${DATASET_DIR}/${OUTPUT_DIR}/AB --name ${OUTPUT_DIR}_pix2pix --model pix2pix --which_model_netG unet_256 --which_direction AtoB --lambda_A 100 --dataset_mode aligned --no_lsgan --no_flip --norm batch --pool_size 0 --display_id=0 --gpu_ids=${GPU_IDS} --batchSize=${BATCH_SIZE} --niter=$3 --niter_decay=0 --save_epoch_freq=$3 --loadSize=256 --fineSize=256 --input_nc=1 --output_nc=1 --slack_freq=${SLACK_FREQ} --no_html --nThreads=${NUM_THREADS} --serial_batches
+printf "done! training\n"
 python send_to_slack.py --msg="학습이 종료되었습니다." --channel=training
 
 
@@ -52,9 +49,14 @@ python send_to_slack.py --msg="학습이 종료되었습니다." --channel=train
 
 
 # upload pth to S3
-aws s3 cp ${CHECK_DIR}/${OUTPUT_DIR}_pix2pix/$3_net_G.pth s3://fontto/data/pths/${UNICODE_A}/${UNICODE_A}_${UNICODE_B}/
-python send_to_slack.py --msg="`printf "[%s_%s/%s_net_G.pth]가 s3에 업로드 되었습니다." $UNICODE_A $UNICODE_B $3`" --channel=training
-
+if [ -d ${CHECK_DIR}/${OUTPUT_DIR}_pix2pix/$3_net_G.pth ]
+then
+  aws s3 cp ${CHECK_DIR}/${OUTPUT_DIR}_pix2pix/$3_net_G.pth s3://fontto/data/pths/${UNICODE_A}/${UNICODE_A}_${UNICODE_B}/
+  python send_to_slack.py --msg="`printf "[%s_%s/%s_net_G.pth]가 s3에 업로드 되었습니다." $UNICODE_A $UNICODE_B $3`" --channel=training
+else
+  python send_to_slack.py --msg="`printf "[%s_%s/%s_net_G.pth]업로드가 실패했습니다." $UNICODE_A $UNICODE_B $3`" --channel=training
+  printf "[%s]를 찾을 수 없습니다." ${CHECK_DIR}/${OUTPUT_DIR}_pix2pix/$3_net_G.pth
+fi
 
 # delete used data for space's sake
 rm -rf ${DATASET_DIR}/${OUTPUT_DIR}
