ca1752d7d0554a32fd98b4ad6bf8f6f60c8bd31b
sjang42 <jexful92@google.com>
2017-11-22 15:54:00 +0900

    test 옵션 추가
    ---
    Redmine Issue Progress / ID / Time :  100% #101 @10m
    ---
    What :
    - DATA_ROOT 변경
    
    Why :
    - 테스트셋 데이터 구조 변경되어서
    ---
    *Signed-off-by: sjang42 <jexjang92@gmail.com>*

--- test.sh
+++ test.sh
@@ -1,7 +1,19 @@
+#!/usr/bin/env bash
 if [ $# -lt 1 ]; then
   echo '$1 <name for trained data>'
   exit
 fi
 
-data_root=./testsets_256/`echo $1 | cut -f 1 -d'_'`
-python test.py --dataroot $data_root --name $1_pix2pix --model test --which_model_netG unet_256 --which_direction AtoB --dataset_mode single --norm batch --how_many 100 --resize_or_crop='' --input_nc=1 --output_nc=1 --loadSize=256 --fineSize=256
+#data_root=./testsets_256/`echo $1 | cut -f 1 -d'_'`
+UNICODE_A=`echo $1 | cut -f 1 -d'_'`
+CHARACTER_A=`printf "\u$UNICODE_A"`
+DATA_ROOT=testsets_256/${CHARACTER_A}/
+
+if [ ! -d ${DATA_ROOT} ]
+then
+  printf "there is no testset [%s]" ${DATA_ROOT}
+  exit
+fi
+
+
+python test.py --dataroot ${DATA_ROOT} --name $1_pix2pix --model test --which_model_netG unet_256 --which_direction AtoB --dataset_mode single --norm batch --resize_or_crop='' --input_nc=1 --output_nc=1 --loadSize=256 --fineSize=256 --gpu_ids=-1 --how_many=100
